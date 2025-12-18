f2c6a795bf0cdb1bcce8d37e24450e984a45961e
GuillemotClement <guillemot.clement@protonmail.com>
2025-12-18 21:47:26 +0100

    feat: add layout for tools

--- frontend/src/pages/Tools/ToolsPage.tsx
+++ frontend/src/pages/Tools/ToolsPage.tsx
@@ -1,15 +1,69 @@
 import { Link, Outlet } from "@tanstack/react-router";
+import { HeartPulse, House, PanelLeftOpen } from "lucide-react";
 
 export default function ToolsPage() {
 	return (
-		<div>
-			tools page
-			<ul>
-				<li>
-					<Link to="/tools/fcm">fcm</Link>
-				</li>
-			</ul>
-			<Outlet />
+		<div className="drawer lg:drawer-open">
+			<input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+			<div className="drawer-content">
+				{/* Navbar */}
+				<nav className="navbar w-full bg-base-300">
+					<label
+						htmlFor="my-drawer-4"
+						aria-label="open sidebar"
+						className="btn btn-square btn-ghost"
+					>
+						{/* Sidebar toggle icon */}
+						<PanelLeftOpen />
+					</label>
+					<div className="px-4">Outils</div>
+				</nav>
+				{/* Page content here */}
+				<div className="py-10 px-4">
+					<Outlet />
+				</div>
+			</div>
+
+			<div className="drawer-side is-drawer-close:overflow-visible">
+				<label
+					htmlFor="my-drawer-4"
+					aria-label="close sidebar"
+					className="drawer-overlay"
+				></label>
+
+				<div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
+					{/* Sidebar content here */}
+
+					<ul className="menu w-full grow">
+						{/* List item */}
+						<li>
+							<Link
+								to="/"
+								className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+								data-tip="Homepage"
+							>
+								{/* Home icon */}
+								<House />
+								<span className="is-drawer-close:hidden">Accueil</span>
+							</Link>
+						</li>
+
+						<li>
+							<Link
+								to="/tools/fcm"
+								className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+								data-tip="Fréquence Cardique"
+							>
+								<HeartPulse />
+
+								<span className="is-drawer-close:hidden">
+									Fréquence Cardiaque
+								</span>
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 }
