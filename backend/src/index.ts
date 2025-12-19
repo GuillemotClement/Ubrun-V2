6797d2795226bb5dc85dbd6f4b6befe09f5cd9b3
GuillemotClement <guillemot.clement@protonmail.com>
2025-12-19 14:56:03 +0100

    feat: add better-auth on backend

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import { Elysia } from "elysia";
+import { authRouter } from "./router/authRouter";
+import { auth } from "./libs/auth";
+import { cors } from '@elysiajs/cors'
+
+const app = new Elysia()
+  .use(
+    cors({
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization']
+    })
+  )
+  .get("/", () => "Hello Elysia")
+  .mount(auth.handler)
+  .use(authRouter)
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
