a5338b2e321e7a69b790d5104e33b06d777ae6e0
GuillemotClement <guillemot.clement@protonmail.com>
2025-12-22 14:37:41 -0800

    feat: push

--- backend/src/@types/club.ts
+++ backend/src/@types/club.ts
@@ -1,5 +1,5 @@
 import type { InferInsertModel, InferSelectModel } from "drizzle-orm/table";
-import { club } from "../db/schema";
+import type { club } from "../db/schema";
 
 export type Club = InferSelectModel<typeof club>;
 export type NewClub = InferInsertModel<typeof club>;
